a67d6a9eacda6853602b8f728173b8f6e15421a7
Harishanth THARMALINGAM <harishanth.tharmalingam@leoo.fr>
2021-01-06 15:16:04 +0100

    [!]-> Fixed searchEngine bugs

--- src/Actions/SearchEngine.js
+++ src/Actions/SearchEngine.js
@@ -1,34 +1,44 @@
-import React from 'react'
 import axios from 'axios'
+import { RESULTS_LOADED, ERROR_LOADED } from './type'
+
 const API_URL = "https://opendata.paris.fr"
 
-export function searchByName(name) {
+export const initialSearch = name => dispatch => {
+  axios.get(API_URL + '/api/records/1.0/search/?dataset=que-faire-a-paris-&q=')
+        //success
+        .then(async newData => dispatch(setResults({bool : true, array : await newData.data.records})))
+        //fail
+        .catch(error => dispatch(setError(error)))
+}
+
+export const searchByName = name => dispatch => {
     axios.get(API_URL + '/api/records/1.0/search/?dataset=que-faire-a-paris-&q=' + name)
-        .then(json => json.data.records.map(result => (
-          {
-            fields: {
-                name: `${result.fields.title}`
-            }
-          })))
-          // Success
-          .then(newData => console.log(newData)) // dispatch ici
-          // FAIL
-          .catch(error => console.log(error)) // dispatch ici
+        //success
+        .then(async newData => dispatch(setResults({bool : true, array : await newData.data.records})))
+        //fail
+        .catch(error => dispatch(setError(error)))
 }
 
 
 // Pas encore fonctionnel
-export function searchByDate(date) {
-    axios.get(API_URL + '/api/records/1.0/search/?dataset=que-faire-a-paris-&refine.modified=' + date)
-        .then(json => json.data.records.map(result => (
-          {
-            fields: {
-                name: `${result.fields.title}`
-            }
-          })))
-          // Success
-          .then(newData => console.log(newData)) // dispatch ici
-          // FAIL
-          .catch(error => console.log(error)) // dispatch ici
+export const searchByDate = date => dispatch => {
+    axios.get(API_URL + '/api/records/1.0/search/?dataset=que-faire-a-paris-&q=&facet=date_start&refine.date_start=' + date)
+      // Success
+        .then(async newData => dispatch(setResults({bool : true, array : await newData.data.records})))
+      // FAIL
+      .catch(error => dispatch(setError(error)))
 }
 
+export const setResults = (res) => { 
+  return {
+    type: RESULTS_LOADED,
+    value : res,
+  }
+}
+
+export const setError = value => {
+  return {
+    type: ERROR_LOADED,
+    value : value
+  }
+}
