ec22c55c9b13d287634ea0a1e5eafd9ee4273c28
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-01-07 09:22:12 +0100

    feat: marker hover

--- src/Components/SideBar/List/index.js
+++ src/Components/SideBar/List/index.js
@@ -3,12 +3,22 @@ import { connect } from 'react-redux';
 import ReactHtmlParser from 'react-html-parser'
 import Cards from './Cards'
 import { Spinner } from 'react-bootstrap'
-
+import {setResults} from '../../../Actions/SearchEngine'
 
 class List extends Component {
   constructor(props) {
     super(props);
 
+  }
+
+  async handleHover(item) {
+    const {ResultsReducer} = this.props;
+    const array = ResultsReducer.results
+    array.filter(element => element.recordid === item.recordid ? element.isActive = true : element.isActive = false)
+    await this.props.setResults({array: array, bool : true,})
+    ResultsReducer.results.map(element => {
+      console.log(element.isActive)
+    })
   }
 
   render() {
@@ -19,7 +29,9 @@ class List extends Component {
         return (      
             <div className="eventScroll">
               {ResultsReducer.results.map((element, i) => (
+                <div onMouseEnter={() => this.handleHover(element)}>
                   <Cards key={i} data={element}/>
+                </div>
               ))}
             </div>
         );
@@ -38,4 +50,4 @@ const mapStateToProps = state => ({
   ResultsReducer : state.ResultsReducer
 })
 
-export default connect(mapStateToProps, {})(List);
+export default connect(mapStateToProps, {setResults})(List);
